e329729b907ee401d7e2a6f3f7fc9d45a9d181da
Mnene <mnene1223@gmail.com>
2026-05-19 16:00:39 +0000

    Not yet fixed

--- src/app/actions/payment-actions.ts
+++ src/app/actions/payment-actions.ts
@@ -1,3 +1,4 @@
+
 'use server';
 
 import { PESAPAL_CONFIG } from '@/lib/pesapal-config';
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
